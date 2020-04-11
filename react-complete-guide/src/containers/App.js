7970f66de38c4d35001e6779713464003954123e
m.goncu <muratlutfigoncu@gmail.com>
2020-04-11 22:04:29 +0300

    removed .DS_store

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -43,7 +43,6 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
